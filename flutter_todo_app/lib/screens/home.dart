ca1dd61ef74da70463682ed75eac4d97322652f7
vinnietito <vincentnjuguna69@gmail.com>
2024-12-22 13:52:31 +0300

    Commits

---
+++ flutter_todo_app/lib/screens/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('ToDo App')),
+      body: Container(
+        child: Text('This is home screen.'),
+      ),
+    );
+  }
